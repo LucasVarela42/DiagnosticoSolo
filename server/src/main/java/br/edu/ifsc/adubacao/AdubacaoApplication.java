9d2be5987fb1c6aca179f25c23ce095928f81482
Lucas Varela <lucaas.varela@gmail.com>
2018-11-26 16:19:14 -0200

    Deploy heroku successful

--- server/src/main/java/br/edu/ifsc/adubacao/AdubacaoApplication.java
+++ server/src/main/java/br/edu/ifsc/adubacao/AdubacaoApplication.java
@@ -19,17 +19,4 @@ public class AdubacaoApplication {
 		SpringApplication.run(AdubacaoApplication.class, args);
 	}
 
-	@Bean
-	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-		config.setAllowedMethods(Collections.singletonList("*"));
-		config.setAllowedHeaders(Collections.singletonList("*"));
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
 }
